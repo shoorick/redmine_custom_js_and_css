22b90cc5f49d5dcea9dfefbcc19eb0fd7454a98f
Alexander Sapozhnikov <shoorick@mail.ru>
2010-10-20 23:46:09 +0600

    Initial commit

---
+++ assets/javascripts/alert.js
@@ -0,0 +1,1 @@
+alert('Here is Custom JS!');
---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_custom_javascript do
+  name 'Redmine Custom Javascript plugin'
+  author 'Alexander Sapozhnikov'
+  description 'This plugin for Redmine allows to execute custom JavaScript on every page'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://shoorick.ru/'
+end

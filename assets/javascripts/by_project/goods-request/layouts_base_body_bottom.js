476294e25063c3d627998a9744c4f35268a9598c
Alexander Sapozhnikov <shoorick@mail.ru>
2010-10-21 15:20:09 +0600

    Script was splitted

---
+++ assets/javascripts/by_project/goods-request/layouts_base_body_bottom.js
@@ -0,0 +1,4 @@
+// Rename labels and links
+$$('#main-menu a.roadmap'  ).shift().innerHTML = 'Кварталы';
+$$('#main-menu a.issues'   ).shift().innerHTML = 'Заявки';
+$$('#main-menu a.new-issue').shift().innerHTML = 'Новая заявка';

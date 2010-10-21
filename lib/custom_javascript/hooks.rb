02fa7006b3472e71e60a8de880dabaf4f147b858
Alexander Sapozhnikov <shoorick@mail.ru>
2010-10-21 15:38:38 +0600

    Improved choosing: * Hooks check existence of project-name/hook-name.js and project-name.css * Added custom stylesheet linking

--- lib/custom_javascript/hooks.rb
+++ lib/custom_javascript/hooks.rb
@@ -2,20 +2,41 @@
 # add content to the page
 class Hooks  < Redmine::Hook::ViewListener
 
+  # Custom styles
+  def view_layouts_base_html_head(context = { })
+    prj = context[:project] || return
+    id = prj.identifier
+    if File.file?(
+      File.dirname(__FILE__) + "/../../assets/stylesheets/by_project/#{id}.css"
+    )
+      stylesheet_link_tag "by_project/#{id}", :plugin => 'redmine_custom_javascript'
+    end
+  end
+
+  # Call javascript if found corresponding file for every project
   def view_layouts_base_body_bottom(context = { })
-    unless User.current.anonymous?
-      javascript_include_tag 'index', :plugin => 'redmine_custom_javascript'
-#      '<script language="JavaScript" src="/plugin_assets/redmine_custom_javascript/javascripts/index.js"></script>'
+    prj = context[:project] || return
+    hookname = 'layouts_base_body_bottom'
+    if User.current.logged?
+      id = prj.identifier
+      if File.file?(
+        File.dirname(__FILE__) + "/../../assets/javascripts/by_project/#{id}/#{hookname}.js"
+      )
+        javascript_include_tag "by_project/#{id}/#{hookname}", :plugin => 'redmine_custom_javascript'
+      end
     end
   end
 
   # Try to add hook to bottom of issue form
   def view_issues_form_details_bottom(context = { })
-    id = context[:project].identifier
+    prj = context[:project] || return
+    id = prj.identifier
+    hookname = 'issues_form_details_bottom'
     if File.file?(
-        File.dirname(__FILE__) + '/../../assets/javascripts/by_project/' + id + '/issues_form_details_bottom.js'
+        File.dirname(__FILE__) + "/../../assets/javascripts/by_project/#{id}/#{hookname}.js"
     )
-      'Found!'
+        javascript_include_tag "by_project/#{id}/#{hookname}", :plugin => 'redmine_custom_javascript'
     end
   end
+
 end
